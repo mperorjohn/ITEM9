1ca78503400420f100530d4c01e8119d7aa847c5
Mperor-John <mperorjohnoyekola@gmail.com>
2023-09-04 13:52:24 +0100

    checkout in progress

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -6,6 +6,7 @@ import {
   Container,
   Flex,
   Heading,
+  Image,
   Stack,
   Text,
 } from "@chakra-ui/react";
@@ -22,23 +23,42 @@ const CheckOut = () => {
     setDefaultCart(defaultCart - 1);
   };
 
+  const ExpressCheckout = [
+    "src/assets/Mastercard-logo.svg.png",
+    "src/assets/Verve_Image.png",
+    "src/assets/Visa-investment-in-Africa.png",
+  ];
+
   return (
     <Stack>
-      <Flex flexDirection={"row"}>
-        <Flex flex={1} bg={"none"} justify={"center"}>
+      <Flex flexDirection={{ base: "column", md: "row" }}>
+        <Flex flex={1} bg={"none"} justify={"center"} borderRadius={"full"}>
           <Container>
-            <Text
-              size={""}
-              textAlign={"center"}
-              fontSize={"4xl"}
-              color={"white"}
-            >
+            <Text textAlign={"center"} fontSize={"4xl"} color={"white"}>
               Complete Order Now
             </Text>
             <Card borderRadius={"none"}>
               <CardHeader>
-                <Text _hover={{color:'orange.200', transition:'0.5s'}} color={"whatsapp.200"} fontWeight={"medium"}>
-                  Information &gt; Payment &gt; Shipping
+                <Text
+                  _hover={{ color: "orange.200", transition: "0.5s" }}
+                  color={"whatsapp.200"}
+                  fontWeight={"medium"}
+                >
+                  Information &gt; Payment &gt; Delivery
+                </Text>
+
+                <Flex gap={6} justify={"center"} border={"2px"}>
+                  {ExpressCheckout.map((image, index) => (
+                    <Image
+                      boxSize={"80px"}
+                      objectFit={"contain"}
+                      key={index}
+                      src={image}
+                    />
+                  ))}
+                </Flex>
+                <Text fontStyle={"italic"} textAlign={"center"}>
+                  Express checkout
                 </Text>
               </CardHeader>
             </Card>
